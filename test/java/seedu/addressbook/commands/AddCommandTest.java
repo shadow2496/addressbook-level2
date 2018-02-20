328573d48d69199fa214b71de5502c74a560a71c
Seunghwan Choi <dadadak4@gmail.com>
2018-02-21 03:10:03 +0800

    Update JUnit tests to reflect the new address format

--- test/java/seedu/addressbook/commands/AddCommandTest.java
+++ test/java/seedu/addressbook/commands/AddCommandTest.java
@@ -58,7 +58,7 @@ public class AddCommandTest {
 
     @Test
     public void addCommand_invalidAddress_throwsException() {
-        final String[] invalidAddresses = { "", " " };
+        final String[] invalidAddresses = { "", " ", "invalid", "in, valid", "in, valid, address" };
         for (String address : invalidAddresses) {
             assertConstructingInvalidAddCmdThrowsException(Name.EXAMPLE, Phone.EXAMPLE, true, Email.EXAMPLE,
                     true, address, true, EMPTY_STRING_LIST);
--- test/java/seedu/addressbook/commands/ViewCommandTest.java
+++ test/java/seedu/addressbook/commands/ViewCommandTest.java
@@ -49,7 +49,7 @@ public class ViewCommandTest {
         ReadOnlyPerson stranger = new Person(new Name("me"),
                                              new Phone("123", true),
                                              new Email("some@hey.go", true),
-                                             new Address("nus", false),
+                                             new Address("4, NUS, #44-44, 444444", false),
                                              new UniqueTagList(Collections.emptySet()));
         List<ReadOnlyPerson> listWithExtraPerson
                 = new ArrayList<ReadOnlyPerson>(listWithAllTypicalPersons);
