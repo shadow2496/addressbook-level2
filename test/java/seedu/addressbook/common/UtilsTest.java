82465b15323dd74f9015caf932036b2a980c687d
Seunghwan Choi <dadadak4@gmail.com>
2018-03-02 00:37:28 +0800

    Add isAnyNull() in UtilsTest

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -4,12 +4,41 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
 public class UtilsTest {
 
+    @Test
+    public void isAnyNull() {
+        // empty list
+        assertNotNull(Utils.isAnyNull());
+
+        // no null objects
+        assertNotNull(1);
+        assertNotNull("");
+        assertNotNull("abc");
+        assertNotNull("abc", "ab", "a");
+        assertNotNull("abc", "abc");
+        assertNotNull("abc", "", "abc", "ABC");
+        assertNotNull("", "abc", "a", "abc");
+        assertNotNull(1, 2);
+        assertNotNull(1, new Integer(1));
+
+        // some null objects
+        assertIsAnyNull((Object) null);
+        assertIsAnyNull(null, 1, new Integer(1));
+        assertIsAnyNull(null, null);
+        assertIsAnyNull(null, "a", "b", null);
+    }
+
+    private void assertIsAnyNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
+    }
+
+    private void assertNotNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
+    }
 
     @Test
     public void elementsAreUnique() throws Exception {
